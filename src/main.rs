137be05acdf2dedbf6804f1b89a75966aedd06b1
Mark Smith <smith.h.mark@gmail.com>
2020-06-16 15:41:10 -0500

    adding a unit test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn greeting() -> &'static str {
+    "Hello, world"
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", greeting());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_greeting() {
+        assert_eq!(greeting(), "Hello, world");
+    }
 }
