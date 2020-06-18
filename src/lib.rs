a69f99922821dde4d92bb490a1373acdfe34c9d6
Mark Smith <smith.h.mark@gmail.com>
2020-06-17 23:29:13 -0500

    adding purchasing logic

--- src/lib.rs
+++ src/lib.rs
@@ -22,16 +22,33 @@ struct Improvement {
 }
 
 #[allow(dead_code)]
-fn subtract(hand: GemPool, cost: GemPool) -> GemPool {
-    let mut result = GemPool::new();
-    for (kk, vv) in hand.iter(){
-        let tmp = match cost.get(kk){
-            Some(&cnt) => vv - cnt,
-            _ => *vv
-        };
-        result.insert(*kk, tmp);
+fn purchase(hand: &GemPool, cost: &GemPool) -> Option<GemPool> {
+    if can_aford(hand, cost) {
+        let mut result = GemPool::new();
+        for (kk, vv) in hand.iter(){
+            let tmp = match cost.get(kk){
+                Some(&cnt) => vv - cnt,
+                _ => *vv
+            };
+            result.insert(*kk, tmp);
+        }
+        Some(result)
+    } else {
+        None
     }
-    result
+}
+
+#[allow(dead_code)]
+fn can_aford(hand: &GemPool, cost: &GemPool) -> bool {
+    let mut have_enough = true;
+    for (kk, need) in cost.iter(){
+        let tmp = match hand.get(kk){
+            Some(&have) => if have >= *need { true } else { false },
+            _ => false
+        };
+        have_enough &= tmp;
+    }
+    have_enough
 }
 
 #[cfg(test)]
@@ -39,16 +56,59 @@ mod tests {
     use super::*;
 
     #[test]
-    fn can_subtract_error_free() {
+    fn afordable() {
         let mut cost = GemPool::new();
         let mut hand = GemPool::new();
-        
-        assert_eq!(cost, hand);
+        cost.insert(GemType::White, 1);
+        hand.insert(GemType::White, 2);
+        assert_eq!(can_aford(&hand, &cost), true);
+        assert_eq!(can_aford(&cost, &hand), false);
+
+        hand.insert(GemType::White, 1);
+        assert_eq!(can_aford(&hand, &cost), true);
+        assert_eq!(can_aford(&cost, &hand), true);
+
+        hand.insert(GemType::Red, 1);
+        assert_eq!(can_aford(&hand, &cost), true);
+        assert_eq!(can_aford(&cost, &hand), false);
+
+        cost.insert(GemType::Blue, 1);
+        assert_eq!(can_aford(&hand, &cost), false);
+        assert_eq!(can_aford(&cost, &hand), false);
+    }
+
+    #[test]
+    fn can_purchase_simple() {
+        let mut cost = GemPool::new();
+        let mut hand = GemPool::new();
         cost.insert(GemType::White, 1);
         hand.insert(GemType::White, 2);
 
-        let rem = subtract(hand, cost);
-        assert_eq!(rem.len(), 1);
-        assert_eq!(rem.contains_key(&GemType::White), true);
+        let res = purchase(&hand, &cost);
+        let valid = match res {
+            Some(rem) => {
+                assert_eq!(rem.len(), 1);
+                assert_eq!(rem.contains_key(&GemType::White), true);
+                true
+            },
+            _ => false
+        };
+        assert!(valid, "expected a Some<GemPool>")
+        
+    }
+
+    #[test]
+    fn cannot_purchase() {
+        let mut cost = GemPool::new();
+        let mut hand = GemPool::new();
+        hand.insert(GemType::White, 1);
+        cost.insert(GemType::White, 2);
+
+        let res = purchase(&hand, &cost);
+        let cant_afford = match res {
+            None => true,
+            _ => false
+        };
+        assert!(cant_afford, "shouldn't be able to buy something that we can't afford")
     }
 }
